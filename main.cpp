7a36d59a1fcdda344892e6950f3899f835227241
jcgranda <jcgranda@uniovi.es>
2019-10-03 18:00:05 +0200

    Añado etiquetas TODO y FIXME

--- main.cpp
+++ main.cpp
@@ -1,114 +1,102 @@
 /*
  * Main.cpp
  *
- *  Created on: 13 sept. 2018
- *      Author: arias
+ *  Created on: Fall 2019
  */
 
-#include <CImg.h>
-#include <math.h>
 #include <stdio.h>
+#include <math.h>
+#include <CImg.h>
+
 using namespace cimg_library;
 
-/**********************************
- * TODO
- * 	- Change the data type returned by CImg.srcImage to adjust the
- * 	requirements of your workgroup
- * 	- Change the data type of the components pointers to adjust the
- * 	requirements of your workgroup
- */
+// Data type for image components
+// FIXME: Change this type according to your group assignment
+typedef float data_t;
+
+const char* SOURCE_IMG      = "bailarina.bmp";
+const char* DESTINATION_IMG = "bailarina2.bmp";
+
 
 int main() {
-	CImg<float> srcImage("bailarina.bmp"); // Open file and object initialization
+	// Open file and object initialization
+	CImg<data_t> srcImage(SOURCE_IMG);
 
-	float *pRcomp, *pGcomp, *pBcomp; // Pointers to the R, G and B components
-	float *pRnew, *pGnew, *pBnew;
-	float *pdstImage; // Pointer to the new image pixels
-	int width, height; // Width and height of the image
-	int nComp; // Number of image components
+	data_t *pRsrc, *pGsrc, *pBsrc; // Pointers to the R, G and B components
+	data_t *pRdest, *pGdest, *pBdest;
+	data_t *pDstImage; // Pointer to the new image pixels
+	uint width, height; // Width and height of the image
+	uint nComp; // Number of image components
 
 
 	/***************************************************
-	 *
-	 * Variables initialization.
-	 * Preparation of the necessary elements for the algorithm
-	 * Out of the benchmark time
-	 *
+	 * TODO: Variables initialization.
+	 *   - Prepare variables for the algorithm
+	 *   - This is not included in the benchmark time
 	 */
 
-	srcImage.display(); // If needed, show the source image
-	width = srcImage.width(); // Getting information from the source image
-	height  = srcImage.height();
-	nComp = srcImage.spectrum(); // source image number of components
+	srcImage.display(); // Displays the source image
+	width  = srcImage.width(); // Getting information from the source image
+	height = srcImage.height();
+	nComp  = srcImage.spectrum(); // source image number of components
 				// Common values for spectrum (number of image components):
 				//  B&W images = 1
 				//	Normal color images = 3 (RGB)
 				//  Special color images = 4 (RGB and alpha/transparency channel)
 
 
-	// Allocate memory space for the pixels of the destination (processed) image 
-	pdstImage = (float *) malloc (width * height * nComp * sizeof(float));
-	if (pdstImage == NULL) {
-		printf("\nMemory allocating error\n");
+	// Allocate memory space for destination image components
+	pDstImage = (data_t *) malloc (width * height * nComp * sizeof(data_t));
+	if (pDstImage == NULL) {
+		perror("Allocating destination image");
 		exit(-2);
 	}
 
-	// Pointers to the RGB arrays of the source image
-	pRcomp = srcImage.data(); // pRcomp points to the R component
-	pGcomp = pRcomp + height * width; // pGcomp points to the G component
-	pBcomp = pGcomp + height * width; // pBcomp points to B component
+	// Pointers to the componet arrays of the source image
+	pRsrc = srcImage.data(); // pRcomp points to the R component array
+	pGsrc = pRsrc + height * width; // pGcomp points to the G component array
+	pBsrc = pGsrc + height * width; // pBcomp points to B component array
 
 	// Pointers to the RGB arrays of the destination image
-	pRnew = pdstImage;
-	pGnew= pRnew + height * width;
-	pBnew= pGnew + height * width;
+	pRdest = pDstImage;
+	pGdest = pRdest + height * width;
+	pBdest = pGdest + height * width;
 
-	/*********************************************
-	 * Algorithm start
-	 *
-	 * Measure initial time
-	 *
-	 *	COMPLETE
-	 *
+
+	/***********************************************
+	 * TODO: Algorithm start.
+	 *   - Measure initial time
 	 */
-
 
 
 	/************************************************
-	 * Algorithm.
-	 * In this example, the algorithm is a components exchange
+	 * FIXME: Algorithm.
+	 * In this example, the algorithm is a components swap
 	 *
 	 * TO BE REPLACED BY YOUR ALGORITHM
 	 */
-
-	for (int i=0; i< width * height; i++){
-		*(pRnew + i) = *(pGcomp + i);
-		*(pGnew + i) = *(pBcomp + i);
-		*(pBnew + i) = *(pRcomp + i);
+	for (uint i = 0; i < width * height; i++){
+		*(pRdest + i) = *(pGsrc + i);  // This is equals to pRdest[i] = pGsrc[i]
+		*(pGdest + i) = *(pBsrc + i);
+		*(pBdest + i) = *(pRsrc + i);
 	}
 
 	/***********************************************
-	 * End of the algorithm
-	 *
-	 * Measure the final time and calculate the time spent
-	 *
-	 * COMPLETE
-	 *
+	 * TODO: End of the algorithm.
+	 *   - Measure the end time
+	 *   - Calculate the elapsed time
 	 */
 
 		
 	// Create a new image object with the calculated pixels
-	// In case of normal color image use nComp=3,
-	// In case of B&W image use nComp=1.
-	CImg<float> dstImage(pdstImage, width, height, 1, nComp);
+	// In case of normal color images use nComp=3,
+	// In case of B/W images use nComp=1.
+	CImg<data_t> dstImage(pDstImage, width, height, 1, nComp);
 
-	// Store the destination image in disk
-	dstImage.save("bailarina2.bmp"); 
+	// Store destination image in disk
+	dstImage.save(DESTINATION_IMG); 
 
-	// Display the destination image
-	dstImage.display(); // If needed, show the result image
-	return(0);
-
+	// Display destination image
+	dstImage.display();
+	return 0;
 }
-
-
